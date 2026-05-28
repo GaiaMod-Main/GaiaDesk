3e6f55eb86585d72ff2408f0849a4103b729d7f4
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-05-27 21:18:15 -0400

    A

--- build-app.sh
+++ build-app.sh
@@ -2,19 +2,10 @@ clear
 echo "Clearing packages"
 rm -rf packager-app
 rm -rf app/build
-rm -rf linux-base
 rm -rf windows-base
-rm -rf penguinmod-linux.zip
-rm -rf penguinmod-windows.zip
-rm -rf penguinmod.github.io
+rm -rf GaiaMod-windows.zip
+rm -rf gaiamod-main.github.io
 
-echo "Downloading Electron - Linux"
-
-#Linux
-curl -L https://github.com/electron/electron/releases/download/v31.3.1/electron-v31.3.1-linux-x64.zip > linux.zip
-unzip linux.zip -d linux-base
-rm -rf linux.zip
-rm -rf linux-base/resources/default_app.asar
 echo "Downloading Electron - Windows"
 #Windows
 curl -L https://github.com/electron/electron/releases/download/v31.3.1/electron-v31.3.1-win32-ia32.zip > windows.zip
@@ -23,127 +14,87 @@ rm -rf windows.zip
 rm -rf windows-base/resources/default_app.asar
 
 #PM base
-echo "Downloading PenguinMod"
+echo "Downloading GaiaMod"
 export NODE_OPTIONS=--openssl-legacy-provider
-read -p "Do you want to use a custom GUI made for the desktop app? Using the vanilla UI could cause issues (Y/N)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    git clone https://github.com/TheShovel/brandless-penguin.git
-    mv brandless-penguin penguinmod.github.io
-else
-    git clone https://github.com/PenguinMod/penguinmod.github.io.git
-fi
-cd penguinmod.github.io
+git clone https://github.com/GaiaMod-Main/gaiamod-main.github.io.git
+cd gaiamod-main.github.io
 git pull
 npm install --force
 
 #VM
 echo "Adding VM"
-read -p "Use modified VM version? (Y/N)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-git clone https://github.com/TheShovel/Stable-PM-VM.git
-mv Stable-PM-VM PenguinMod-Vm
-else
-git clone https://github.com/PenguinMod/PenguinMod-Vm.git
-fi
-cd PenguinMod-Vm
+git clone https://github.com/GaiaMod-Main/GaiaMod-Vm.git
+cd GaiaMod-Vm
 git pull
 npm install --force
 cd ..
-cp -R PenguinMod-Vm node_modules
+cp -R GaiaMod-Vm node_modules
 rm -rf node_modules/scratch-vm
-mv node_modules/PenguinMod-Vm node_modules/scratch-vm
+mv node_modules/GaiaMod-Vm node_modules/scratch-vm
 
 #BLOCKS
 echo "Adding Blocks"
-read -p "Use modified Blocks version? (Y/N)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-git clone https://github.com/TheShovel/Stable-PM-Blocks.git
-mv Stable-PM-Blocks PenguinMod-Blocks
-else
-git clone https://github.com/PenguinMod/PenguinMod-Blocks.git
-fi
-cd PenguinMod-Blocks
+git clone https://github.com/GaiaMod-Main/GaiaMod-Blocks.git
+cd GaiaMod-Blocks
 git pull
 npm install --force
 cd ..
-cp -R PenguinMod-Blocks node_modules
+cp -R GaiaMod-Blocks node_modules
 rm -rf node_modules/scratch-blocks
-mv node_modules/PenguinMod-Blocks node_modules/scratch-blocks
+mv node_modules/GaiaMod-Blocks node_modules/scratch-blocks
 
 #RENDERER
 echo "Adding Renderer"
-read -p "Use safe Renderer version? (Y/N)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    git clone https://github.com/TheShovel/Stable-PM-Render.git
-else
-    git clone https://github.com/PenguinMod/PenguinMod-Render.git
-fi
-mv Stable-PM-Render PenguinMod-Render
-cd PenguinMod-Render
+git clone https://github.com/GaiaMod-Main/GaiaMod-Render.git
+cd GaiaMod-Render
 git pull
 npm install --force
 cd ..
-cp -R PenguinMod-Render node_modules
+cp -R GaiaMod-Render node_modules
 rm -rf node_modules/scratch-render
-mv node_modules/PenguinMod-Render node_modules/scratch-render
+mv node_modules/GaiaMod-Render node_modules/scratch-render
 
 #PAINT
 echo "Adding Paint"
-git clone https://github.com/PenguinMod/PenguinMod-Paint.git
-cd PenguinMod-Paint
+git clone https://github.com/GaiaMod-Main/GaiaMod-Paint.git
+cd GaiaMod-Paint
 git pull
 npm install --force
 cd ..
-cp -R PenguinMod-Paint node_modules
+cp -R GaiaMod-Paint node_modules
 rm -rf node_modules/scratch-paint
-mv node_modules/PenguinMod-Paint node_modules/scratch-paint
+mv node_modules/GaiaMod-Paint node_modules/scratch-paint
 
-echo "Building PenguinMod"
+echo "Building GaiaMod"
 npm run --silent build
 sleep 5s
 cp -R build ../app
-git clone https://github.com/PenguinMod/PenguinMod-Packager.git
-cd PenguinMod-Packager
+git clone https://github.com/GaiaMod-Main/GaiaMod-Packager.git
+cd GaiaMod-Packager
 git pull
 npm install --force
 cd ..
 #vm
-cp -R PenguinMod-Vm PenguinMod-Packager/node_modules
-rm -rf PenguinMod-Packager/node_modules/scratch-vm
-mv PenguinMod-Packager/node_modules/PenguinMod-Vm PenguinMod-Packager/node_modules/scratch-vm
+cp -R GaiaMod-Vm GaiaMod-Packager/node_modules
+rm -rf GaiaMod-Packager/node_modules/scratch-vm
+mv GaiaMod-Packager/node_modules/GaiaMod-Vm GaiaMod-Packager/node_modules/scratch-vm
 #blocks
-cp -R PenguinMod-Blocks PenguinMod-Packager/node_modules
-rm -rf PenguinMod-Packager/node_modules/scratch-blocks
-mv PenguinMod-Packager/node_modules/PenguinMod-Blocks PenguinMod-Packager/node_modules/scratch-blocks
+cp -R GaiaMod-Blocks GaiaMod-Packager/node_modules
+rm -rf GaiaMod-Packager/node_modules/scratch-blocks
+mv GaiaMod-Packager/node_modules/GaiaMod-Blocks GaiaMod-Packager/node_modules/scratch-blocks
 #renderer
-cp -R PenguinMod-Render PenguinMod-Packager/node_modules
-rm -rf PenguinMod-Packager/node_modules/scratch-render
-mv PenguinMod-Packager/node_modules/PenguinMod-Render PenguinMod-Packager/node_modules/scratch-render
+cp -R GaiaMod-Render GaiaMod-Packager/node_modules
+rm -rf GaiaMod-Packager/node_modules/scratch-render
+mv GaiaMod-Packager/node_modules/GaiaMod-Render GaiaMod-Packager/node_modules/scratch-render
 #paint
-cp -R PenguinMod-Paint PenguinMod-Packager/node_modules
-rm -rf PenguinMod-Packager/node_modules/scratch-paint
-mv PenguinMod-Packager/node_modules/PenguinMod-Paint PenguinMod-Packager/node_modules/scratch-paint
-cd PenguinMod-Packager
+cp -R GaiaMod-Paint GaiaMod-Packager/node_modules
+rm -rf GaiaMod-Packager/node_modules/scratch-paint
+mv GaiaMod-Packager/node_modules/GaiaMod-Paint GaiaMod-Packager/node_modules/scratch-paint
+cd GaiaMod-Packager
 npm run --silent build
 cd ..
 cd ..
-cp -R penguinmod.github.io/PenguinMod-Packager/dist app/build/packager-app
-cp -R app linux-base/resources/
+cp -R gaiamod-main.github.io/GaiaMod-Packager/dist app/build/packager-app
 cp -R app windows-base/resources/
-mv linux-base/electron linux-base/penguinmod-desktop
-mv windows-base/electron.exe windows-base/penguinmod-desktop.exe
-read -p "Do you want to zip the Electron packages? (Y/N)" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    zip -r penguinmod-linux.zip linux-base
-    zip -r penguinmod-windows.zip windows-base
-fi
+mv windows-base/electron.exe windows-base/GaiaMod-desktop.exe
+    zip -r GaiaMod-windows.zip windows-base
